a8de8007dcaad60c0b0e7134c4caaa51e6020f42
saad moumou <saad.moumou.coder@gmail.com>
2023-09-19 23:25:55 +0100

    partie functions

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 
@@ -12,15 +13,27 @@ public class Main {
 //        List square = number.stream().map(x -> x*x).collect(Collectors.toList());
 //        System.out.println(square);
 
-        List<Person> people = List.of(
-                new Person("Saad Moumou", Gender.Male , 23, Citys.Youssoufia),
-                new Person("ikram Moumou", Gender.Female , 15, Citys.Youssoufia),
-                new Person("Natalia Boursha", Gender.Male , 30, Citys.Casa)
-        );
+//        List<Person> people = List.of(
+//                new Person("Saad Moumou", Gender.Male , 23, Citys.Youssoufia),
+//                new Person("ikram Moumou", Gender.Female , 15, Citys.Youssoufia),
+//                new Person("Natalia Boursha", Gender.Male , 30, Citys.Casa)
+//        );
+//
+//        people.stream()
+//                .filter(x -> Gender.Female.equals(x.getGender()))
+//                .map(x -> x.getFullName().toUpperCase())
+//                .forEach(System.out::println);
 
-        System.out.println(people);
+        int increment1 = incrementNumberDeclarative.apply(1);
+        System.out.println(increment1);
+        int increment2 = incrementNumberImperative(1);
+        System.out.println(increment2);
 
 
 
+    }
+    static Function<Integer,Integer> incrementNumberDeclarative = x -> ++x;
+    static int incrementNumberImperative(int x){
+        return ++x;
     }
 }
